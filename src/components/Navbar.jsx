0b19c71bc085a081050ad7ae66c2d45a8afc4e41
Chethana Akarshani <wawegedarachethana@gmail.com>
2026-06-09 18:23:16 +0530

    Improve mobile responsiveness and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
+
     <nav className="navbar">
 
       <a href="#home" className="logo">
-        <span className="logo-dot"></span>
-        <span className="logo-first">Nimasha</span>
-        <span className="logo-last">Savindi</span>
+        NimashaSavindi
       </a>
 
-    <ul className="nav-links">
+      <div
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+
         <li><a href="#home">About</a></li>
+
         <li><a href="#skills">Skills</a></li>
+
         <li><a href="#education">Education</a></li>
-        <li><a href="#projects">Projects</a></li> 
+
+        <li><a href="#projects">Projects</a></li>
+
         <li><a href="#certifications">Certifications</a></li>
+
         <li><a href="#contact">Contact</a></li>
-    </ul>
+
+      </ul>
 
     </nav>
+
   );
 }
 
